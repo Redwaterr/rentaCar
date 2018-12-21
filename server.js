979e46999909a65890f1ed9cbee070180235731b
Ugur <ugur@localhost.localdomain>
2018-12-22 01:34:24 +0300

    second commit

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const routesAPI = require("./app_api/route/index");
 
-import Cars from "./app_api/models/cars";
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
@@ -14,11 +14,11 @@ app.use(bodyParser.json());
 app.use("/api",routesAPI)
 
 
-app.listen(4000,(err) => {
+app.listen(port,(err) => {
     if(err) {
         return console.log("Bağlanamadık");
     }
-    console.log("4000 protunaki servera bağlantı sağlanıldı.");
+    console.log(port+" portundaki servera bağlantı sağlanıldı.");
 })
 
 
